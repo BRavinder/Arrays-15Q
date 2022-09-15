bb51d665f9bdf77108b7bee5c06e1330632814be
BRavinder <105974685+BRavinder@users.noreply.github.com>
2022-09-15 11:31:10 +0530

    Time: 1 ms (79.64%), Space: 51.1 MB (47.02%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+		
+        int sum = nums.length*(nums.length+1)/2;
+        
+		for (int num : nums){
+             sum -= num;
+        }
+           
+        
+		return sum;
+    }
+}
+
+//==>Time complexity o(n)
+//==>Space complexity o(1)
